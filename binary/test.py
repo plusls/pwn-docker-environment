b0d5e8d6ee8d845be5e3c004d040a62ce4e32056
plulsls <plusls@qq.com>
2021-11-01 20:19:48 +0800

    comment

--- binary/test.py
+++ binary/test.py
@@ -2,7 +2,7 @@ from pwn import *
 from docker_debug import *
 
 #context.terminal = ['tmux', 'splitw', '-h']
-context.terminal = ['wt.exe', '-w', '0', 'split-pane', 'wsl.exe', '-e']
+#context.terminal = ['wt.exe', '-w', '0', 'split-pane', 'wsl.exe', '-e']
 # context.log_level = 'debug'
 def main():
     debug_env = DockerDebug('ubuntu-1604')
